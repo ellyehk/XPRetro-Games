142fce797259a3d9ac109c8e32900f46bb3d462f
ellye <143988533+ellyehk@users.noreply.github.com>
2025-09-27 21:08:59 -0600

    Delete src directory

--- src/php/cerrar_sesion.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-setcookie("sesion_token", "", time() - 3600, "/"); // eliminar cookie
-session_destroy();
-echo json_encode(["success" => true]);
-?>
--- src/php/obtener_generos.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once("conexion.php");
-$result = $conn->query("SELECT Id, Genero AS Nombre FROM genero ORDER BY Id");
-echo json_encode($result->fetch_all(MYSQLI_ASSOC));
